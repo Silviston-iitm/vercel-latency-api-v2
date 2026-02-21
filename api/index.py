1e6c1e70b07b5fe7981e425b643510a889cfc2af
silvi <silviston@gmail.com>
2026-02-21 15:37:33 +0530

    new8

--- api/index.py
+++ api/index.py
@@ -34,7 +34,7 @@ def options_handler():
         headers={
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "POST, OPTIONS",
-            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
         },
     )
 
